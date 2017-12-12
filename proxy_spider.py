9de423a4ccc4352a3066c0528e50abfc51fb4a8b
dingweihua <dingweihuaic@126.com>
2017-12-12 13:47:46 +0800

    validte proxy available or not

--- proxy_spider.py
+++ proxy_spider.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Author: 布丁(dwh@dobechina.com)
+# Author: 布丁(dingweihuaic@gmail.com)
 #
 # Created: 2017/12/12 下午12:06
 
@@ -11,7 +11,8 @@ from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0'}
 url = 'http://www.xicidaili.com/nn/'
-n = 50
+validate_url = 'https://www.baidu.com/'
+n = 10
 with open('host.txt', 'w') as fp:
     for num in range(n):
         s = requests.get(url + str(num+1), headers=headers)
@@ -21,6 +22,13 @@ with open('host.txt', 'w') as fp:
             ipp = i.select('td')
             if len(ipp) < 3:
                 continue
-            ip = ipp[1].text
-            port = ipp[2].text
-            fp.write('{}:{}\n'.format(ip, port))
+            host = '{}:{}'.format(ipp[1].text, ipp[2].text)
+            proxies = {'http': host}
+            # 校验proxy是否可用
+            try:
+                s = requests.get(validate_url, proxies=proxies)
+                if s.status_code == 200:
+                    fp.write(host)
+                    fp.write('\n')
+            except Exception as e:
+                print(e)
